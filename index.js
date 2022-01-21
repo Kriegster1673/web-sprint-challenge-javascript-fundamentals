4b1ebbdbbc65fb3899b3fc7a64abd732656fd4a2
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-01-21 13:01:56 -0600

    Actually finished step 3

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ CuboidMaker.prototype.volume = function (){
   2 * (length * width + length * height + width * height)  */
 
 CuboidMaker.prototype.surfaceArea = function (){
-  return (this.length * this.width) + (this.length * this.height) + (this.width * this.height)
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
 }
 
 
